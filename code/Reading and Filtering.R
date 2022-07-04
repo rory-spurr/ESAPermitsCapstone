d2fc0cec1cac1d68ed0993536991dce79800f6fa
Rory Spurr <rjspurr5@live.com>
2022-07-04 12:51:24 -0700

    Update Reading and Filtering.R
    - Eliminated dependency on "here" package in an effort to move to projects (easier for collaboration and sharing)
     - Cleaned up initial filtering of permit data into one long pipe chain and reduced global variables made

--- code/Reading and Filtering.R
+++ code/Reading and Filtering.R
@@ -2,26 +2,20 @@
 # Script to read in and filter data
 library(tidyverse)
 library(sf)
-library(here)
 
 # West Coast region read in -> with Alana's filters
-wcr <- read_csv(here("data", "WCRpermitBiOp_allregns_all_years__7Jan2022.csv"))
-wcr_rev <- wcr %>% 
-  filter(PermitStatus == "Issued")
-wcr_rev1 <- wcr_rev %>% 
-  filter(DateIssued >"2012-01-01")
-wcr_rev2 <- wcr_rev1 %>% 
-  filter(DateExpired >= Sys.Date()) #DateField >= Sys.Date() puts it to the date of the system
-wcr_rev3 <- wcr_rev2 %>% 
-  filter(ResultCode == c("NMFS 10a1A Salmon","4d", "NMFS BiOp DTA", "Tribal 4d"))
-wcr_rev4 <- wcr_rev3 %>%
+wcr.init <- read_csv("data/WCRpermitBiOp_allregns_all_years__7Jan2022.csv")
+
+wcr <- wcr.init %>% 
+  filter(PermitStatus == "Issued") %>%
+  filter(DateIssued >"2012-01-01") %>%
+  filter(DateExpired >= Sys.Date()) %>% #DateField >= Sys.Date() puts it to the date of the system
+  filter(ResultCode == c("NMFS 10a1A Salmon","4d", "NMFS BiOp DTA", "Tribal 4d")) %>%
   mutate(LifeStage = recode(LifeStage,
                             "Smolt" = "Juvenile",
                             "Fry" = "Juvenile",
                             "Larvae" = "Juvenile",
-                            "Subadult" = "Adult"))
-
-wcr_rev5 <- wcr_rev4 %>% 
+                            "Subadult" = "Adult")) %>%
   mutate(CommonName = recode(CommonName,
                              "Salmon, coho" = "coho salmon",
                              "Steelhead" = "steelhead", #steelhead respawn
@@ -32,8 +26,15 @@ wcr_rev5 <- wcr_rev4 %>%
                              "Sturgeon, green" = "green sturgeon",
                              "Rockfish, Canary" = "canary rockfish",
                              "Rockfish, Bocaccio" = "bocaccio",
-                             "Rockfish, Yelloweye" = "yelloweye rockfish"))
-
+                             "Rockfish, Yelloweye" = "yelloweye rockfish")) %>%
+  mutate(HUCNumber = recode(HUCNumber,
+                            `18020103` = 18020156,
+                            `18020109` = 18020163,
+                            `18020112` = 18020154,
+                            `18020118` = 18020154,
+                            `18040005` = 18040012,
+                            `18060001` = 18060015,
+                            `18060012` = 18060006)) # check notes below:
 # recoding HUCs to account for HUCs that were altered or moved
 # broken hucs are 18020103, 18020109, 18020112, 18020118,
 # 18040005, 18060001, 18060012
@@ -47,19 +48,10 @@ wcr_rev5 <- wcr_rev4 %>%
 # 18060001 = 18060015 # split between 18050006 as well, arbitrarily picked
 # 18060012 = 18060006 # chose this over Monterrey Bay as population is South-Central Cal Coast
 
-wcr_rev5 <- wcr_rev5 %>% 
-  mutate(HUCNumber = recode(HUCNumber,
-                            `18020103` = 18020156,
-                            `18020109` = 18020163,
-                            `18020112` = 18020154,
-                            `18020118` = 18020154,
-                            `18040005` = 18040012,
-                            `18060001` = 18060015,
-                            `18060012` = 18060006))
-
-adults <- wcr_rev5 %>% 
+# Seperate DF by life stage
+adults <- wcr %>% 
   filter(LifeStage == "Adult")
-juvenile <- wcr_rev5 %>% 
+juvenile <- wcr %>% 
   filter(LifeStage == "Juvenile")
 
 # Geodatabases and other Spatial Objects
