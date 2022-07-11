09e7b4af09b3a0174a8cb9fe515b84abfcb865b4
asantan8 <asantan7@uw.edu>
2022-07-11 13:43:30 -0700

    Exploring R Shiny dashboard abilities

--- code/R Shiny App Tut.R
+++ code/R Shiny App Tut.R
@@ -177,5 +177,94 @@ server <- function(input, output) {
 #' 
 #' 
 #' 
-#' 
-#' 
+#-----
+#' R Shiny Dashboard Functions
+ install.packages("shinydashboard")
+ library(shinydashboard)
+ library (shiny)
+## ui.R ##
+dashboardPage(
+  dashboardHeader(),
+  dashboardSidebar(),
+  dashboardBody()
+)
+#
+#
+## app.R ##
+ui <- dashboardPage(
+  dashboardHeader(),
+  dashboardSidebar(),
+  dashboardBody()
+)
+
+server <- function(input, output) { }
+
+shinyApp(ui, server)
+#
+#
+## app.R blank dashboard ##
+ui <- dashboardPage(
+  dashboardHeader(title = "Basic dashboard"),
+  dashboardSidebar(),
+  dashboardBody(
+    # Boxes need to be put in a row (or column)
+    fluidRow(
+      box(plotOutput("plot1", height = 250)),
+      
+      box(
+        title = "Controls",
+        sliderInput("slider", "Number of observations:", 1, 100, 50)
+      )
+    )
+  )
+)
+
+server <- function(input, output) {
+  set.seed(122)
+  histdata <- rnorm(500)
+  
+  output$plot1 <- renderPlot({
+    data <- histdata[seq_len(input$slider)]
+    hist(data)
+  })
+}
+
+shinyApp(ui, server)
+#
+#
+## creating sidebar ##
+#'There are two parts that need to be done. 
+#'First, you need to add menuItems to the sidebar, with appropriate tabNames.
+#'In the body, add tabItems with corrsponding values for tabName
+
+## Sidebar content
+dashboardSidebar(
+  sidebarMenu(
+    menuItem("Dashboard", tabName = "dashboard", icon = icon("dashboard")),
+    menuItem("Widgets", tabName = "widgets", icon = icon("th"))
+  )
+)
+
+## Body content
+dashboardBody(
+  tabItems(
+    # First tab content
+    tabItem(tabName = "dashboard",
+            fluidRow(
+              box(plotOutput("plot1", height = 250)),
+              
+              box(
+                title = "Controls",
+                sliderInput("slider", "Number of observations:", 1, 100, 50)
+              )
+            )
+    ),
+    
+    # Second tab content
+    tabItem(tabName = "widgets",
+            h2("Widgets tab content")
+    )
+  )
+)
+#
+#
