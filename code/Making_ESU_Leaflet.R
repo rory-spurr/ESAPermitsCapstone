ecaef47bafecd22726d22f63f4bb8af8b6483cb6
Rory Spurr <rjspurr5@live.com>
2022-07-28 11:41:48 -0700

    Added popup labels for each ESU/HUC8

--- code/Making_ESU_Leaflet.R
+++ code/Making_ESU_Leaflet.R
@@ -1,26 +1,7 @@
 # Making the ESU Leafelet
 # Rory Spurr
 
-source("code/Reading and Filtering.R")
-source("code/NOAA Permitting Team Code.R")
-
-library(shiny)
-library(leaflet)
-
-wcr <- wcr %>%
-  rename_population() %>%
-  rename_takeaction() %>%
-  create_totalmorts()
-
-ESUs <- unique(wcr$Species)
-ESUs
-
-ESUdf <- aggregate(wcr$ExpTake, 
-                   by = list(wcr$HUCNumber, wcr$Species, wcr$LifeStage),
-                   FUN = sum) # aggregate total expected take by HUC
-names(ESUdf) <- c("huc8", "ESU", "Lifestage", "ExpTake") # rename columns
-ESU.spatial <- right_join(wbd.hucs, ESUdf, by = "huc8") # join with spatial data
-ESU.spatial <- st_transform(ESU.spatial, crs = 4326)
+source("code/PreAppCode-1.R")
 
 ui <- fluidPage(
   selectInput(inputId = "DPS", label = "Choose an ESU to View",
@@ -35,15 +16,20 @@ server <- function(input, output){
   })
   output$map <- renderLeaflet({
     leaflet(ESU.spatial) %>% 
-      addTiles()
+      addProviderTiles(providers$Stamen.TerrainBackground)
   })
   observe({
-    pal <- colorNumeric(palette = "viridis", domain = filteredData()$ExpTake)
-    
+    pal <- colorBin(palette = "viridis",
+                    domain = filteredData()$ExpTake,
+                    bins = 3, pretty = F)
+
     leafletProxy("map", data = filteredData()) %>%
         clearShapes() %>%
         addPolygons(
-          fillColor = ~pal(filteredData()$ExpTake)
+          fillColor = ~pal(filteredData()$ExpTake),
+          popup = ~labels,
+          highlight = highlightOptions(color = "white",
+                                       bringToFront = T)
           )
   })
 }
---
+++ code/PreAppCode-1.R
@@ -0,0 +1,35 @@
+source("code/Reading and Filtering.R")
+source("code/NOAA Permitting Team Code.R")
+
+library(shiny)
+library(leaflet)
+library(leaflet.extras)
+library(leaflet.providers)
+library(htmlwidgets)
+
+wcr <- wcr %>%
+  rename_population() %>%
+  rename_takeaction() %>%
+  create_totalmorts()
+
+ESUs <- unique(wcr$Species)
+ESUs
+
+ESUdf <- aggregate(wcr$ExpTake, 
+                   by = list(wcr$HUCNumber, wcr$Species, wcr$LifeStage),
+                   FUN = sum) # aggregate total expected take by HUC
+names(ESUdf) <- c("huc8", "ESU", "Lifestage", "ExpTake") # rename columns
+ESU.spatial <- right_join(wbd.hucs, ESUdf, by = "huc8") # join with spatial data
+ESU.spatial <- st_transform(ESU.spatial, crs = 4326)
+
+# Make labels for Leaflet map popup
+ESU.spatial$labels <- paste0(
+    "<strong> Name: </strong>",
+    ESU.spatial$name, "<br/> ",
+    "<strong> HUC 8: </strong>",
+    ESU.spatial$huc8, "<br/> ",
+    "<strong> Expected Take (# of fish): </strong> ",
+    ESU.spatial$ExpTake, "<br/> "
+  ) %>%
+    lapply(htmltools::HTML)
+
