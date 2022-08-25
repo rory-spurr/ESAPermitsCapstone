3e03d4ffc8fb2c8c2954c3920f07f560344247e8
Rory Spurr <rjspurr5@live.com>
2022-08-25 10:59:02 -0700

    fixed issue with data table not displaying hatchery fish

--- Making_ESU_Leaflet.R
+++ Making_ESU_Leaflet.R
@@ -43,7 +43,7 @@ server <- function(input, output){
     wcr %>%
       filter(Species == input$DPS) %>%
       filter(LifeStage == input$lifestage) %>% 
-      filter(Production == input$Prod)
+      filter(Prod == input$Prod)
   })
   output$map <- renderLeaflet({
     leaflet(ESU.spatial) %>% 
