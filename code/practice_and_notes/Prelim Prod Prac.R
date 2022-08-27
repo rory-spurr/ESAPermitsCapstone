c7eed47766fde8b6d59b8ebc0d61808988505d08
asantan8 <asantan7@uw.edu>
2022-08-27 11:51:52 -0700

    Update Prelim Prod Prac.R

--- code/practice_and_notes/Prelim Prod Prac.R
+++ code/practice_and_notes/Prelim Prod Prac.R
@@ -25,7 +25,7 @@ wcr_GT <- wcr_spatial %>%
 wcr_GT <- subset(wcr_GT, permit != FALSE) 
 #==============================================================
 #Setting up palette for leaflet 1
-pal <- colorBin("viridis", domain = wcr_GT$states) 
+#pal <- colorBin("viridis", domain = wcr_GT$states) #in progress: finding better way to bin data
 #==============================================================
 # Creating Map 1
 labels <- paste0(
@@ -54,4 +54,43 @@ leaflet(wcr_GT) %>%
     highlight = highlightOptions(color = "white", bringToFront = T)) %>% 
   setView(lng = -124.072971, lat = 43.458,
           zoom = 5)
+#==============================================================
+#Setting up leaflet 2 - Take Action
+wcr_TA <- wcr_spatial %>% 
+  group_by(huc8, name, states, Species, TakeAction, Organization) %>% 
+  count(permit = ResultCode)
+wcr_TA <- subset(wcr_TA, permit != FALSE) 
+#==============================================================
+# Creating Map 2
+labels <- paste0(
+  "<strong> State: </strong> ",
+  wcr_TA$states, "<br/> ",
+  "<strong> HUC: </strong> ",
+  wcr_TA$name, "<br/> ",
+  "<strong> Organization: </strong> ",
+  wcr_TA$Organization, "<br/> ",
+  "<strong> Gear Type: </strong> ",
+  wcr_TA$TakeAction, "<br/> ",
+  "<strong> Species: </strong> ",
+  wcr_GT$Species, "<br/> ") %>%
+  lapply(htmltools::HTML)
 
+leaflet(wcr_TA) %>% 
+  addProviderTiles(providers$Stamen.TonerLite) %>% 
+  addEasyButton(easyButton(
+    icon="fa-crosshairs", title="Locate Me",
+    onClick=JS("function(btn, map){ map.locate({setView: true}); }"))) %>%
+  addPolygons(
+    fillColor = "blue", 
+    color = "white",
+    fillOpacity = 0.9, 
+    label = ~labels,
+    highlight = highlightOptions(color = "white", bringToFront = T)) %>% 
+  setView(lng = -124.072971, lat = 43.458,
+          zoom = 5)
+#==============================================================
+#Setting up tables to display data
+#in progress
+#==============================================================
+#Integrating into Shiny Dashboard
+#in progress
