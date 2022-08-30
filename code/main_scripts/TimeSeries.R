a0d41e8eca0d40c969dc68a22aef22b846b737da
asantan8 <asantan7@uw.edu>
2022-08-30 14:24:40 -0700

    Update TimeSeries.R

--- code/main_scripts/TimeSeries.R
+++ code/main_scripts/TimeSeries.R
@@ -7,10 +7,66 @@ library(sf)
 library(dplyr)
 library(tidyverse)
 library(leaflet)
+library(NMFSResPermits)
 sf_use_s2(FALSE)
 #==============================================================
 #Sourcing Script
-setwd("~/GitHub/ESA_Permits_Capstone")
+#setwd("~/GitHub/ESA_Permits_Capstone")
 source(paste(getwd(), "/code/dependencies/Reading and Filtering.R", sep = ""))
 #==============================================================
-#Setting up data
+#Setting up data - Changing time factor
+a<-as.factor(wcr_act$AnnualTimeStart)
+b<-strptime(a,format="%Y-%m-%d") 
+Year <- format(as.Date(b, format ="%Y-%m-%d" ), "%Y")
+Year <- as.data.frame(Year)
+wcr.v <- cbind(wcr_act, Year)
+#==============================================================
+#Setting up data - Filtering and recoding 
+wcr.v <- wcr.v %>% 
+  filter(ResultCode == c("NMFS 10a1A Salmon","4d", "NMFS BiOp DTA", "Tribal 4d")) %>% 
+  mutate(CommonName = recode(CommonName,
+                             "Salmon, coho" = "coho salmon",
+                             "Steelhead" = "steelhead", #steelhead respawn
+                             "Eulachon" = "eulachon",
+                             "Salmon, Chinook" = "Chinook salmon",
+                             "Salmon, chum" = "chum salmon",
+                             "Salmon, sockeye" = "sockeye salmon",
+                             "Sturgeon, green" = "green sturgeon",
+                             "Rockfish, Canary" = "canary rockfish",
+                             "Rockfish, Bocaccio" = "bocaccio",
+                             "Rockfish, Yelloweye" = "yelloweye rockfish")) %>%
+  mutate(LifeStage = recode(LifeStage,
+                            "Smolt" = "Juvenile",
+                            "Fry" = "Juvenile",
+                            "Larvae" = "Juvenile",
+                            "Subadult" = "Adult")) %>%
+  filter(LifeStage != "Spawned Adult/ Carcass") %>% 
+  mutate(Species = paste(Population, CommonName, sep = " ")) %>% 
+  mutate(Species = recode(Species, 
+                          "NA sockeye salmon" = "Columbia River sockeye salmon")) %>% 
+  mutate(Prod = recode(Production, 
+                       "Natural" = "Natural", 
+                       "Listed Hatchery" = "Listed Hatchery", 
+                       "Listed Hatchery, Clipped and Intact" = "Listed Hatchery",  
+                       "Listed Hatchery Intact Adipose" = "Listed Hatchery", 
+                       "Listed Hatchery Adipose Clip" = "Listed Hatchery", 
+                       "Unlisted Hatchery" = "Unlisted Hatchery")) %>%
+  filter(Prod != "Unlisted Hatchery") %>%
+  filter(Prod != "All") %>% 
+  filter(Species != "NA")
+#==============================================================
+#Setting up data - Creating authtake and totalmorts 
+wcr.v <-wcr.v %>% 
+  create_totalmorts() %>%
+  order_table() %>% 
+  replace_na(list(ExpTake = 0, ActTake = 0, TotalMorts = 0, ActMort = 0))
+wcr.v <- wcr.v %>% 
+  mutate(AuthTake = ExpTake + IndMort)
+#==============================================================
+df <- aggregate(wcr.v$AuthTake, 
+                by = list(wcr.v$CommonName, wcr.v$Species, wcr.v$LifeStage, wcr.v$Prod, 
+                          wcr.v$Year, wcr.v$TotalMorts), FUN = sum)
+names(df) <- c("CommonName", "ESU", "LifeStage", "Production", "Year", "TotalMorts")             
+
+  
+
