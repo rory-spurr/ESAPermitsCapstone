31522753cb581a50067fceace2aa15e20b0d4b9a
asantan8 <asantan7@uw.edu>
2022-10-18 18:18:02 -0700

    Update TS PreAppCode.R

--- code/dependencies/TS PreAppCode.R
+++ code/dependencies/TS PreAppCode.R
@@ -22,8 +22,8 @@ df <- aggregate(wcr.v$ExpTake,
                           wcr.v$Year, wcr.v$TotalMorts), FUN = sum) 
 
 names(df) <- c("CommonName", "ResultCode", "ActMort", "ActTake", "TakeAction", "ESU", "LifeStage", "Production", "Year", "TotalMorts", "ExpTake")
-df <- df %>% 
-  filter(LifeStage == "Adult", ESU == "Puget Sound Chinook salmon", Production == "Natural") # comment out when Anne is no longer working with the code
+#df <- df %>% 
+  #filter(LifeStage == "Adult", ESU == "Puget Sound Chinook salmon", Production == "Natural") # comment out when Anne is no longer working with the code
 #==============================================================
 #Summing each variable by year
 YT <-df %>%
@@ -47,14 +47,20 @@ df <- merge(df, df2, by = c("Year", "ESU", "Production", "LifeStage"))
 #==============================================================
 #Creating proportions
 # df <- df %>%
-#   mutate(ActualT = df$ActTake/df$Yearly_Take) %>%
-#   mutate(AuthT = df$ExpTake/df$Yearly_ET) %>%
-#   mutate(ActualM = df$ActMort/df$Yearly_Mort) %>%
-#   mutate(AuthM = df$TotalMorts/df$Yearly_TM)
+#   mutate(PropT = df$Yearly_Take/df$Yearly_ET) %>% 
+#   mutate(PropM = df$Yearly_Mort/df$Yearly_TM)
+
+# df <- df %>%
+#   mutate(ActualT = (df$ActTake/df$Yearly_Take)*100) %>%
+#   mutate(AuthT = (df$ExpTake/df$Yearly_ET)*100) %>%
+#   mutate(ActualM = (df$ActMort/df$Yearly_Mort)*100) %>%
+#   mutate(AuthM = (df$TotalMorts/df$Yearly_TM)*100)
 #==============================================================
 #Merging columns
- df_T <- pivot_longer(df, Yearly_ET:Yearly_Take, names_to = "Take", values_to = "ProportionT")
- df_M <- pivot_longer(df, Yearly_TM:Yearly_Mort, names_to = "Mortality", values_to = "ProportionM")
+ df_T <- pivot_longer(df, Yearly_Take:Yearly_ET, names_to = "Take", values_to = "ProportionT")
+ df_M <- pivot_longer(df, Yearly_Mort:Yearly_TM, names_to = "Mortality", values_to = "ProportionM")
+ # df_T <- pivot_longer(df, AuthT:ActualT, names_to = "Take", values_to = "ProportionT")
+ # df_M <- pivot_longer(df, AuthM:ActualM, names_to = "Mortality", values_to = "ProportionM")
 # #==============================================================
 # #Merging data sets
 df <- merge(df_M, df_T, by = c("Year", "ESU", "Production", "LifeStage", 
@@ -62,13 +68,16 @@ df <- merge(df_M, df_T, by = c("Year", "ESU", "Production", "LifeStage",
                                "CommonName",
                                "ResultCode", "ActMort", "ActTake", 
                                "TakeAction"))
+                              # ,
+                              # "Yearly_Take", "Yearly_ET", "Yearly_Mort",
+                              # "Yearly_TM"))
 #==============================================================
 #Replacing NaN or NA or Inf with 0
 df[is.na(df)] <- 0
 #==============================================================
- ggplot(data = df, aes(y = ProportionT, x = Year, fill = Take) ) +
-   geom_bar(stat = "identity", position = "stack")+
-   scale_fill_viridis(discrete = T) +
-   labs(x = "Year", y = "Take", title = "Total Authorized Take vs Reported Take over Time")
- ggplotly(tooltip = c("y", "x"))
+ # ggplot(data = df, aes(y = ProportionT, x = Year, fill = Take) ) +
+ #   geom_bar(stat = "identity", position = "stack")+
+ #   scale_fill_viridis(discrete = T) +
+ #   labs(x = "Year", y = "Take", title = "Total Authorized Take vs Reported Take over Time")
+ # ggplotly(tooltip = c("y", "x"))
 
