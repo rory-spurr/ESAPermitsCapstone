00bab6d1af97e16671127b4ba7c0bce77d8b58f2
Rory Spurr <rjspurr5@live.com>
2022-11-01 16:59:18 -0700

    moved these as more work had to be done before the paclage would run them successfully

--- code/dependencies/Reading and Filtering.R
+++ code/dependencies/Reading and Filtering.R
@@ -15,89 +15,11 @@ sf_use_s2(FALSE)
 # Permit Data
 # =================================================================================
 # West Coast region read in -> with Alana's filters
-wcr.init <- read_csv("data/WCRpermitBiOp_allregns_all_years_18Aug2022.csv")
 
 
-wcr <- wcr.init %>% 
-  filter(PermitStatus == "Issued") %>%
-  filter(DateIssued >"2012-01-01") %>%
-  filter(DateExpired >= Sys.Date()) %>% #DateField >= Sys.Date() puts it to the date of the system
-  filter(ResultCode %in% c("NMFS 10a1A Salmon","4d", "NMFS BiOp DTA", "Tribal 4d")) %>%
-  mutate(LifeStage = recode(LifeStage,
-                            "Smolt" = "Juvenile",
-                            "Fry" = "Juvenile",
-                            "Larvae" = "Juvenile",
-                            "Subadult" = "Adult")) %>%
-  mutate(CommonName = recode(CommonName,
-                             "Salmon, coho" = "coho salmon",
-                             "Steelhead" = "steelhead", #steelhead respawn
-                             "Eulachon" = "eulachon",
-                             "Salmon, Chinook" = "Chinook salmon",
-                             "Salmon, chum" = "chum salmon",
-                             "Salmon, sockeye" = "sockeye salmon",
-                             "Sturgeon, green" = "green sturgeon",
-                             "Rockfish, Canary" = "canary rockfish",
-                             "Rockfish, Bocaccio" = "bocaccio",
-                             "Rockfish, Yelloweye" = "yelloweye rockfish")) %>%
-  mutate(HUCNumber = recode(HUCNumber,
-                            `18020103` = 18020156,
-                            `18020109` = 18020163,
-                            `18020112` = 18020154,
-                            `18020118` = 18020154,
-                            `18040005` = 18040012,
-                            `18060001` = 18060015,
-                            `18060012` = 18060006)) %>% 
-  mutate(Species = paste(Population, CommonName, sep = " ")) %>% 
-  mutate(Prod = recode(Production, 
-                       "Natural" = "Natural", 
-                       "Listed Hatchery" = "Listed Hatchery", 
-                       "Listed Hatchery, Clipped and Intact" = "Listed Hatchery",  
-                       "Listed Hatchery Intact Adipose" = "Listed Hatchery", 
-                       "Listed Hatchery Adipose Clip" = "Listed Hatchery", 
-                       # "Listed Hatchery and Natural Origin" = "All", # Only applies to abundance data
-                       "Unlisted Hatchery" = "Unlisted Hatchery")) %>%
-  filter(Prod != "Unlisted Hatchery") %>%
-  filter(Prod != "All") %>% 
-  filter(TakeAction != "Observe/Harass") %>%
-  filter(TakeAction != "Observe/Sample Tissue Dead Animal") %>%
-  filter(TakeAction != "Unknown")
+wcr <- permitFilter(WestCoastPermitData)
 
-
-wcr_act <- read_csv("data/WCRPermitBiOp_Pass report data 4d and S10_18Aug2022.csv")
-wcr_act <- wcr_act %>%
-  filter(ResultCode %in% c("NMFS 10a1A Salmon","4d", "NMFS BiOp DTA", "Tribal 4d")) %>% 
-  mutate(LifeStage = recode(LifeStage,
-                            "Smolt" = "Juvenile",
-                            "Kelt" = "Juvenile",
-                            "Yearling" = "Juvenile",
-                            "Sub-Yearling" = "Juvenile",
-                            "Egg" = "Juvenile",
-                            "Fry" = "Juvenile",
-                            "Larvae" = "Juvenile",
-                            "Subadult" = "Adult",
-                            "Spawned Adult/ Carcass" = "Adult")) %>%
-  mutate(CommonName = recode(CommonName,
-                             "Salmon, coho" = "coho salmon",
-                             "Steelhead" = "steelhead", #steelhead respawn
-                             "Eulachon" = "eulachon",
-                             "Salmon, Chinook" = "Chinook salmon",
-                             "Salmon, chum" = "chum salmon",
-                             "Salmon, sockeye" = "sockeye salmon",
-                             "Sturgeon, green" = "green sturgeon",
-                             "Rockfish, Canary" = "canary rockfish",
-                             "Rockfish, Bocaccio" = "bocaccio",
-                             "Rockfish, Yelloweye" = "yelloweye rockfish")) %>%
-  mutate(Species = paste(Population, CommonName, sep = " ")) %>% 
-  mutate(Prod = recode(Production, 
-                       "Natural" = "Natural", 
-                       "Listed Hatchery" = "Listed Hatchery", 
-                       "Listed Hatchery, Clipped and Intact" = "Listed Hatchery",  
-                       "Listed Hatchery Intact Adipose" = "Listed Hatchery", 
-                       "Listed Hatchery Adipose Clip" = "Listed Hatchery",
-                       "Unlisted Hatchery" = "Unlisted Hatchery")) %>%
-  filter(Prod != "Unlisted Hatchery") %>% 
-  filter(TakeAction != "Observe/Harass") %>%
-  filter(TakeAction != "Observe/Sample Tissue Dead Animal")
+wcr_act <- reportFilter(WestCoastReportData)
 
 # check notes below:
 # recoding HUCs to account for HUCs that were altered or moved
