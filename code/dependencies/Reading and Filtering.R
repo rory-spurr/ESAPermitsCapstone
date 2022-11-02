ebf6cab268d44378e3148cb50c875cef2369e3c5
Rory Spurr <rjspurr5@live.com>
2022-11-01 17:08:06 -0700

    changed file name to match function

--- code/dependencies/Reading and Filtering.R
+++ code/dependencies/Reading and Filtering.R
@@ -21,6 +21,8 @@ wcr <- permitFilter(WestCoastPermitData)
 
 wcr_act <- reportFilter(WestCoastReportData)
 
+wbd.hucs <- WestCoastHUC8
+
 # check notes below:
 # recoding HUCs to account for HUCs that were altered or moved
 # broken hucs are 18020103, 18020109, 18020112, 18020118,
@@ -35,40 +37,34 @@ wcr_act <- reportFilter(WestCoastReportData)
 # 18060001 = 18060015 # split between 18050006 as well, arbitrarily picked
 # 18060012 = 18060006 # chose this over Monterrey Bay as population is South-Central Cal Coast
 
-# Separate DF by life stage
-adults <- wcr %>% 
-  filter(LifeStage == "Adult")
-juveniles <- wcr %>% 
-  filter(LifeStage == "Juvenile")
 
-# =================================================================================
-# Reading in abundance Data
-# =================================================================================
-abund <- read_csv("data/Abundance_2022-03-17.csv")
-abund <- abund %>%
-  mutate(LifeStage = recode(LifeStage,
-                            "Subadult" = "Juvenile")) %>%
-  mutate(Production = recode(Production,
-                             "Listed Hatchery Intact Adipose" = "Listed Hatchery",
-                             "Listed Hatchery Adipose Clip" = "Listed Hatchery"))
+# # =================================================================================
+# # Reading in abundance Data
+# # =================================================================================
+# abund <- read_csv("data/Abundance_2022-03-17.csv")
+# abund <- abund %>%
+#   mutate(LifeStage = recode(LifeStage,
+#                             "Subadult" = "Juvenile")) %>%
+#   mutate(Production = recode(Production,
+#                              "Listed Hatchery Intact Adipose" = "Listed Hatchery",
+#                              "Listed Hatchery Adipose Clip" = "Listed Hatchery"))
 # =================================================================================
 # Spatial Data
 # =================================================================================
 # Reading in HUC 8 shape file
-wbd.hucs <- read_sf("data/WCR_HUC8/WCR_HUC8.shp")
-wbd.hucs$huc8 <- as.double(wbd.hucs$huc8)
 
-# state boundary shape files
-state.bound <- read_sf("data/cb_2018_us_state_20m/cb_2018_us_state_20m.shp")
-wcr.bound <- state.bound %>%
-  filter(NAME == "Washington" | NAME == "Oregon" |
-           NAME == "California" | NAME == "Idaho")
+
+# # state boundary shape files
+# state.bound <- read_sf("data/cb_2018_us_state_20m/cb_2018_us_state_20m.shp")
+# wcr.bound <- state.bound %>%
+#   filter(NAME == "Washington" | NAME == "Oregon" |
+#            NAME == "California" | NAME == "Idaho")
 
 # Puget Sound areas Shapefile
-PS_bound <- read_sf("data/WAPSP_Nearshore_Credits_Marine_Basins/Nearshore_MarineBasins_wm.shp")
+# PS_bound <- read_sf("data/WAPSP_Nearshore_Credits_Marine_Basins/Nearshore_MarineBasins_wm.shp")
 
 # Joining Permit data and spatial data
-wcr_spatial <- right_join(x = wbd.hucs, y = wcr, by = c("huc8" = "HUCNumber"))
+# wcr_spatial <- right_join(x = wbd.hucs, y = wcr, by = c("huc8" = "HUCNumber"))
 
 # ESU spatial Data
 # esuBound <- read_sf("data/esu_boundaries.shp") 
