121a23a7345ab13dfa0d1d4e88fbeb7a8e322f1b
Rory Spurr <rjspurr5@live.com>
2022-11-30 11:39:34 -0800

    changed so tabBox only appears on home tab

--- code/main_scripts/CapProjShinyDash.R
+++ code/main_scripts/CapProjShinyDash.R
@@ -7,18 +7,19 @@ ui <- dashboardPage(
   dashboardSidebar(
     sidebarMenu(
       menuItem("Home", tabName = "home", icon = icon("home")), #info-sign can be another option
-      menuItem("Map", tabName = "map", icon = icon("map-marker", lib = "glyphicon")), #globe can be another option
+      menuItem("Map", tabName = "map", icon = icon("globe", lib = "glyphicon")), #globe can be another option
       menuItem("Time Series", tabName = "time series", icon = icon("time", lib = "glyphicon"))
     )
 ), 
   dashboardBody(
-    tabBox(title = "Getting Started",
-           id = "tabset1", height = "1000px", width = "auto",
-           tabPanel("Background", "Here we will put backghround on WCR/Permits/ESA"),
-           tabPanel("How it works", "Here we will display a video on how it works"),
-           tabPanel("Disclaimer", "Here we will discuss limitations/caveats of app")),
     tabItems(
-      tabItem(tabName = "home"),
+      tabItem(tabName = "home",
+        tabBox(title = "Getting Started",
+        id = "tabset1", height = "1000px", width = "auto",
+        tabPanel("Background", "Here we will put backghround on WCR/Permits/ESA"),
+        tabPanel("How it works", "Here we will display a video on how it works"),
+        tabPanel("Disclaimer", "Here we will discuss limitations/caveats of app")),   
+      ),
       tabItem(tabName = "map"),
       tabItem(tabName = "time series")
     )
