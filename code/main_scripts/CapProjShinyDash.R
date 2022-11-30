c82f4de7cd7b9284a7bc37c9aa0cdaf859f1cee7
asantan8 <asantan7@uw.edu>
2022-11-29 23:37:31 -0800

    getting base of dashboard set up

--- code/main_scripts/CapProjShinyDash.R
+++ code/main_scripts/CapProjShinyDash.R
@@ -2,11 +2,32 @@ library(shiny)
 library(shinydashboard)
 
 ui <- dashboardPage(
-  dashboardHeader(),
-  dashboardSidebar(),
-  dashboardBody()
+  dashboardHeader(title = "Visualizing ESA-Listed Fish Research",
+                  titleWidth = 350),
+  dashboardSidebar(
+    sidebarMenu(
+      menuItem("Home", tabName = "home", icon = icon("home")), #info-sign can be another option
+      menuItem("Map", tabName = "map", icon = icon("map-marker", lib = "glyphicon")), #globe can be another option
+      menuItem("Time Series", tabName = "time series", icon = icon("time", lib = "glyphicon"))
+    )
+), 
+  dashboardBody(
+    tabBox(title = "Getting Started",
+           id = "tabset1", height = "1000px", width = "auto",
+           tabPanel("Background", "Here we will put backghround on WCR/Permits/ESA"),
+           tabPanel("How it works", "Here we will display a video on how it works"),
+           tabPanel("Disclaimer", "Here we will discuss limitations/caveats of app")),
+    tabItems(
+      tabItem(tabName = "home"),
+      tabItem(tabName = "map"),
+      tabItem(tabName = "time series")
+    )
+  )
 )
+server <- function(input, output) { 
+  output$tabset1Selected <- renderText({
+  input$tabset1
+})}
 
-server <- function(input, output) { }
 
 shinyApp(ui, server)
