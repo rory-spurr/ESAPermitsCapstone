a477c227bdd86d343f512246dba69ee6a26be10f
Rory Spurr <rjspurr5@live.com>
2022-11-30 11:52:26 -0800

    created map UI

--- code/main_scripts/CapProjShinyDash.R
+++ code/main_scripts/CapProjShinyDash.R
@@ -7,10 +7,10 @@ ui <- dashboardPage(
   dashboardSidebar(
     sidebarMenu(
       menuItem("Home", tabName = "home", icon = icon("home")), #info-sign can be another option
-      menuItem("Map", tabName = "map", icon = icon("globe", lib = "glyphicon")), #globe can be another option
+      menuItem("Authorized Take Map", tabName = "takeMap", icon = icon("globe", lib = "glyphicon")), #globe can be another option
       menuItem("Time Series", tabName = "time series", icon = icon("time", lib = "glyphicon"))
     )
-), 
+  ), 
   dashboardBody(
     tabItems(
       tabItem(tabName = "home",
@@ -20,8 +20,27 @@ ui <- dashboardPage(
         tabPanel("How it works", "Here we will display a video on how it works"),
         tabPanel("Disclaimer", "Here we will discuss limitations/caveats of app")),   
       ),
-      tabItem(tabName = "map"),
-      tabItem(tabName = "time series")
+      tabItem(tabName = "takeMap",
+        fluidRow(
+          box(
+            title = "Controls",
+            radioButtons(inputId = "lifestage", label = "Choose a lifestage",
+              choices = c("Adult", "Juvenile")),
+            radioButtons(inputId = "Prod", label = "Choose an Origin",
+              choices = c("Natural", "Listed Hatchery")),
+            radioButtons(inputId = "displayData", label = "Choose data to display",
+              choices = c("Total Take", "Lethal Take")),
+            selectInput(inputId = "DPS", label = "Choose an ESU to View",
+              choices = levels(wcr$Species), multiple = F)
+          ),
+          box(
+            leafletOutput("map")
+          ),
+          box(
+            dataTableOutput("wcr_table")
+          ),
+        )
+      )
     )
   )
 )
