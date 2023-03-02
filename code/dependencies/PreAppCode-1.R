d45f7545819ffb385857adc6737fe7421e7a09fc
Rory Spurr <rjspurr5@live.com>
2023-03-01 16:51:32 -0800

    commented and cleaned

--- code/dependencies/PreAppCode-1.R
+++ code/dependencies/PreAppCode-1.R
@@ -1,24 +1,26 @@
-# Rory Spurr
-# Script #2 to run before Shiny App
 # =================================================
+# Rory Spurr                                      
+# Script #1 to run before Shiny App               
+
 # Summary:
-# Creates a new data table for display under the Leaflet map in the Shiny app.
+# Creates a data table for display under the Leaflet map in the Shiny app.
 # Table only includes fields that are of interest, while omitting those that aren't 
-# Creates FW/SW column for table as well
+# Creates a water type column for the table as well
 # =================================================
 
-source("code/dependencies/Reading and Filtering.R")
-source("code/dependencies/NOAA Permitting Team Code.R")
+
+# =================================================
+# Load in packages
 library(DT) # package that helps with data table manipulation
-# install.packages("naniar")
 library(naniar) # helps with replacing NA values
 
 # Package made by Diana Dishman at NOAA, new users will have to download the package from Github
-# by un-commenting and running lines 23:24.
-#
+# by un-commenting and running lines 17:18
 # library(devtools)
 # devtools::install_github("DianaDishman-NOAA/NMFSResPermits")
 library(NMFSResPermits)
+# =================================================
+
 
 # =================================================
 # Use functions from NMFSResPermits package to organize data and create columns
@@ -27,43 +29,67 @@ wcr <- wcr %>%
   rename_takeaction() %>%
   create_totalmorts() %>%
   order_table()
-
 # =================================================
 
-wcr <- wcr %>%
-  replace_with_na(replace = list(WaterbodyName = "N/A",
-                                 BasinName = "N/A"))
 
+# =================================================
+# Creating the location field for the table. This is based on a hierarchical sort 
+# of different location based fields from the raw data. When researchers apply
+# for permits they often use different fields to describe their location.
+# The highest level (largest in terms of geographic scope) is used unless it
+# is an NA value, than the next highest non-NA value is used.
+
+# replaces "N/A" character values with NA
+wcr <- wcr %>%
+  replace_with_na(
+    replace = list(WaterbodyName = "N/A",
+                   BasinName = "N/A")
+  ) 
+
+# Each loop checks each value, if it returns NA it replaces it with the next value in the 
+# hierarchy. 
 Location <- wcr$WaterbodyName
 for (i in 1:nrow(wcr)){
   if(is.na(Location[i] == T)){
     Location[i] <- wcr$BasinName[i]
   }
 }
+
 for (i in 1:nrow(wcr)){
   if(is.na(Location[i] == T)){
     Location[i] <- wcr$StreamName[i]
   }
 }
-wcr <- cbind(wcr, Location)
+
+wcr <- cbind(wcr, Location) # bind the locations to the table
+
 wcr <- wcr %>%
-  replace_with_na(replace = list(Location = "."))
+  replace_with_na(replace = list(Location = ".")) # replaces "." values with NA
+
+# adds the last available location based column if all other values have been NA
+# LocationDescription is (usually) the longest/most detailed, hence why it is at the 
+# bottom of the hierarchy
 for (i in 1:nrow(wcr)){
   if(is.na(wcr$Location[i] == T)){
     wcr$Location[i] <- wcr$LocationDescription[i]
   }
-}
+} 
+
 
 # ===================================================
-# Creating a FW/SW column
-# ===================================================
-Locations <- wcr$Location
+# Creating a water type column that uses keywords to place locations into
+# a freshwater or saltwater category.
+
+Locations <- wcr$Location # grab locations
+
+# Create list of keywords used to identify a place as saltwater or freshwater
 SW_strings <- c("Sound", "Bay", "Ocean", "Strait", "Admiralty",
                 "Whidbey", "Canal", "shelf", "Cape", "estuary", 
                 "Estuary","lagoon", "Lagoon", "Delta",
                 "estuarine")
 FW_strings <- c("Lake", "stream", "freshwaters", "Columbia")
 
+# This function assigns water types based on matching the keywords specified above
 assignWaterType <- function(x, strings1 = SW_strings, strings2 = FW_strings){
   result <- "empty"
     ifelse(str_detect(x, paste(strings1, collapse = "|")) & str_detect(x, paste(strings2, collapse = "|")),
@@ -74,8 +100,10 @@ assignWaterType <- function(x, strings1 = SW_strings, strings2 = FW_strings){
 }
 assignWaterType(x = Locations[41]) # testing the function
 
+#create the column
 SW_FW <- sapply(Locations, assignWaterType)
 
+# only binds the column if it doesn't already exist
 if (length(wcr$SW_FW) > 0){
   wcr <- wcr[,1:31]
   wcr <- cbind(wcr, SW_FW)
@@ -97,9 +125,9 @@ wcr4App <- wcr %>%
   mutate(HUCNumber = as.character(HUCNumber)) %>%
   replace_na(list(HUCNumber = "No Data"))
 
+# table for checking values to ensure the sorting function works
 # check <- table(wcr4App$Location, wcr4App$SW_FW)
-# write.csv(check, "docs/waterTypeCheck.csv") # table for checking values to ensure the
-# sorting function works
+# write.csv(check, "docs/waterTypeCheck.csv") 
 
 
 
