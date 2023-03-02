a9f494f67dcce3e2374ed6eaf036af6d8129c53f
Rory Spurr <rjspurr5@live.com>
2023-03-01 17:07:07 -0800

    cleaned and commented

--- code/dependencies/PreAppCode-3.R
+++ code/dependencies/PreAppCode-3.R
@@ -1,8 +1,16 @@
+# =================================================
 # Rory Spurr
-# Goal of this script is to create large polygons that describe all of the 
-# different HUC8's where you could potentially find a given ESU
+# Script #3 to be run before the Shiny app
 
-# Filter the species we want
+# Summary:
+# Creates large polygons that describe all of the 
+# different HUC8's where you could potentially find a given ESU or DPS
+# =================================================
+
+
+# =================================================
+# Filter the species we want from data set describing where 
+# these species can be found
 ESUBasins <- ESUBasins %>%
   filter(Species %in% c("Eulachon", "Salmon, Chinook", "Salmon, chum",
                      "Salmon, coho", "Salmon, sockeye", "Steelhead",
@@ -10,7 +18,7 @@ ESUBasins <- ESUBasins %>%
   mutate(HUC8 = as.double(HUC8)) %>% # Did not read in as correct data type - fixed here
   mutate(Species = recode(Species,
                           "Salmon, coho" = "coho salmon",
-                          "Steelhead" = "steelhead", #steelhead respawn
+                          "Steelhead" = "steelhead", 
                           "Eulachon" = "eulachon",
                           "Salmon, Chinook" = "Chinook salmon",
                           "Salmon, chum" = "chum salmon",
@@ -21,24 +29,26 @@ ESUBasins <- ESUBasins %>%
                           "Rockfish, Yelloweye" = "yelloweye rockfish")) # naming consistency with permit data
   
 
-ESUBasinsSpatial <- left_join(ESUBasins, wbd.hucs, by = c("HUC8" = "huc8")) %>%
-  st_as_sf() %>%
+ESUBasinsSpatial <- left_join(ESUBasins, wbd.hucs, by = c("HUC8" = "huc8")) %>% # join with spatial data
+  st_as_sf() %>% # converts to sf object
   mutate(DPS = paste(`Population/Stock`, Species, sep = " ")) # create ESU/DPS column (same unique values 
                                                               # as permit data)
 
-DPSs <- unique(ESUBasinsSpatial$DPS)
+DPSs <- unique(ESUBasinsSpatial$DPS) # says DPS here, but also encompasses ESUs
 nDPS <- length(DPSs)
 sf_use_s2(use_s2 = F) # have to shut this off so loop works (due to some vertices being used in multiple shapes)
 
 temp <- ESUBasinsSpatial %>% filter(DPS == DPSs[1])
 temp <- st_union(temp)
 esuBound <- temp
+
+# loop to combine all polygons for each ESU/DPS -> creates ESU boundary polygons
 for (i in 2:nDPS){
   temp <- ESUBasinsSpatial %>% filter(DPS == DPSs[i])
   temp <- st_union(temp)
   esuBound <- rbind(esuBound, temp)
   rm(temp)
-} # loop to combine all polygons for each DPS -> creates ESU boundary polygons
+} 
 
 
 # This chunk re-formats, provides informative column names 
